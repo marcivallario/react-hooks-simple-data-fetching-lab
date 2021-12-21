51f4efe472cedaa5e2ab77eee49da8389e293323
Marci Vallario <marcivallario@gmail.com>
2021-12-21 16:59:45 -0500

    commit tree 0dd44990e3d628ec5ebb1acb1cbdc3e29c93c610

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [ imgSrc, setImgSrc ] = useState('');
+    
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(resp => resp.json())
+        .then(data => {
+            setImgSrc(data.message);
+        })
+    }, [])
+    
+    if (!imgSrc) {
+        return <p>Loading...</p>
+    } 
+    else {
+        return <img src={imgSrc} alt="A Random Dog" />
+    }
+}
+
+export default App;
